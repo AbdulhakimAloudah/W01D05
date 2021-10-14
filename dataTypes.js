0c4adfbdc6d6dcb05f3c3a39de165a7f0c36f3f5
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-10-14 16:51:08 +0300

    d

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+let x = 6;
+let a = "ha";
+let q = [4, 8];
+let r = { n: "hakeem", age: 29 };
+console.log(x);
+console.log(a);
+console.log(q);
+console.log(r);
+
+console.log(typeof x);
+console.log(typeof a);
+console.log(typeof q);
+console.log(typeof r);
+
+//---
+ali = [9, "ok", "dk"]; //opject
+console.log(typeof ali);
+//
+let m = null;
+let g;
--- myVariables.js
+++ myVariables.js
@@ -1,8 +1,22 @@
-let language = "JavaScript"
-let score = 10
-console.log(language + 10);
 
-let length = 5
-let width = 5
-area  
+//First Task
+//1
+let language = "JavaScript";
+let score = 10;
+console.log("language" + 10);
+
+//2
+let length = 5;
+let width = 5;
+area = length * width;
+console.log(area);
+
+//3
+let num1 = 6;
+let num2 = 2;
+console.log(num1 / num2);
+
+//Second Task
+
+
 
